f285059d6bac0ee2a5bb2a8242a8722cb2c6f280
Karol Mendza <karol.mendza@codino.pl>
2019-11-02 21:02:19 +0100

    Initial commit

---
+++ src/main/java/copy/base/CopyBaseApplication.java
@@ -0,0 +1,13 @@
+package copy.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopyBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/copy/base/CopyBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package copy.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
